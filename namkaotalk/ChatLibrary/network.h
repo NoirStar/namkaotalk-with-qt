882ff5f510f55bd5113e3d7a4a6d62a086c0b14c
tree <sky_9233@naver.com>
2025-04-29 21:47:58 +0900

    network 라이브러리 소켓 클래스 정의

--- namkaotalk/ChatLibrary/network.h
+++ namkaotalk/ChatLibrary/network.h
@@ -1,16 +1,64 @@
 #pragma once
 
+#include <winsock2.h>
+#include <mswsock.h>
+#include <utility>
+#include <stdexcept>
+#include <memory>
+
 namespace network {
 
-class TcpSocket {
-public:
-	TcpSocket() = default;
-	~TcpSocket() noexcept = default;
-	bool connect(const char* host, int port);
-	void disconnect();
-	bool send(const char* data, size_t size);
-	bool receive(char* buffer, size_t size);
+class WinSocket {
 
+public:
+	explicit WinSocket(int af, int type, int protocol, LPWSAPROTOCOL_INFOW lpProtocolInfo, GROUP g, DWORD dwFlags) {
+		socket_ = WSASocket(af, type, protocol, lpProtocolInfo, g, dwFlags);
+		if (socket_ == INVALID_SOCKET) {
+			throw std::runtime_error("Failed to create socket");
+		}
+	};
+	~WinSocket() noexcept {
+		if (socket_ != INVALID_SOCKET)
+			closesocket(socket_);
+	};
+
+	WinSocket(const WinSocket&) = delete;
+	WinSocket& operator=(const WinSocket&) = delete;
+	WinSocket(WinSocket&& rhs) noexcept 
+		: socket_(std::exchange(rhs.socket_, INVALID_SOCKET)) {}
+	WinSocket& operator=(WinSocket&& rhs) noexcept {
+		if (this != &rhs) {
+			if (socket_ != INVALID_SOCKET)
+				closesocket(socket_);
+			socket_ = std::exchange(rhs.socket_, INVALID_SOCKET);
+		}
+		return *this;
+	}
+
+	SOCKET get() const noexcept { return socket_; }
+
+private:
+	SOCKET socket_ = INVALID_SOCKET;
+};
+
+class IocpSocket {
+public:
+	explicit IocpSocket(WinSocket socket) : socket_(std::move(socket)) {}
+	~IocpSocket() noexcept = default;
+
+	IocpSocket(const IocpSocket&) = delete;
+	IocpSocket& operator=(const IocpSocket&) = delete;
+	IocpSocket(IocpSocket&& rhs) noexcept = default;
+	IocpSocket& operator=(IocpSocket&& rhs) noexcept = default;
+
+	SOCKET get() const noexcept { return socket_.get(); }
+
+	void BindToIocp(HANDLE hIocp, ULONG_PTR key);
+	bool PostSend(WSABUF* buf, OVERLAPPED* overlapped);
+	bool PostRecv(WSABUF* buf, OVERLAPPED* overlapped);
+
+private:
+	WinSocket socket_;
 };
 
 } // namespace network
--- namkaotalk/TestNamkaoTalk/test.cpp
+++ namkaotalk/TestNamkaoTalk/test.cpp
@@ -2,42 +2,10 @@
 #include "../ChatLibrary/network.h"
 
 // Testcase for ChatLibrary
-TEST(ChatLibraryTest, Network_Connect) {
-  network::TcpSocket socket;
-  
-  EXPECT_TRUE(socket.connect("localhost", 8080));
-}
+TEST(ChatLibraryTest, IocpSocket_CreateSocket) {
+	network::IocpSocket iocpSocket{
+		network::WinSocket(AF_INET, SOCK_STREAM, IPPROTO_TCP, nullptr, 0, WSA_FLAG_OVERLAPPED)
+	};
 
-TEST(ChatLibraryTest, Network_Disconnect) {
-  network::TcpSocket socket;
-
-  socket.connect("localhost", 8080);
-  socket.disconnect();
-
-  // Assuming disconnect doesn't return a value
-  EXPECT_NO_THROW(socket.disconnect());
-}
-
-TEST(ChatLibraryTest, Network_Send) {
-  network::TcpSocket socket;
-  socket.connect("localhost", 8080);
-  const char* message = "Hello, World!";
-
-  EXPECT_TRUE(socket.send(message, strlen(message)));
-}
-
-TEST(ChatLibraryTest, Network_Receive) {
-  network::TcpSocket socket;
-  socket.connect("localhost", 8080);
-  char buffer[1024];
-  EXPECT_TRUE(socket.receive(buffer, sizeof(buffer)));
-}
-
-TEST(ChatLibraryTest, Network_Send_Receive) {
-  network::TcpSocket socket;
-  socket.connect("localhost", 8080);
-  const char* message = "Hello, World!";
-  char buffer[1024];
-  EXPECT_TRUE(socket.send(message, strlen(message)));
-  EXPECT_TRUE(socket.receive(buffer, sizeof(buffer)));
+	EXPECT_NE(iocpSocket.get(), INVALID_SOCKET);
 }
